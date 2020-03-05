1abe3855bf54d92e1253b5ad3140d37e32d1331b
Hannah Drury <hannah_drury@yahoo.com>
2020-03-05 19:08:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  result = array.find do |n|
-    cheese_types.include?(n)
+  result = array.find do |n| # find returns the element in the array
+    cheese_types.include?(n) # include returns true or false, does the array cheese_types include n?
   end
   
 end
