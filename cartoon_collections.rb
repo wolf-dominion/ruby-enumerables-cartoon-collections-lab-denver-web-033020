9f45fdde32ca5031a67e828795219bf472b21d6b
Hannah Drury <hannah_drury@yahoo.com>
2020-03-05 18:36:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,6 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese()
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
