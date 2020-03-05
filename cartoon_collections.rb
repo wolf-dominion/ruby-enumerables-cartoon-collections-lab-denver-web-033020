9a848bd0d25c8682b967d430528d75af800a17b8
Hannah Drury <hannah_drury@yahoo.com>
2020-03-05 18:20:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet()
-  
+def summon_captain_planet(array)
+  array.each_with_index do |item, index|
+  puts "#{index+1} #{item}"
+end
 end
 
 def long_planeteer_calls()
